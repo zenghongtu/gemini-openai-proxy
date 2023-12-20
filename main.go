85c2be045f41a2a7be78e80b425828a7c782b5d5
jasonzeng <zenghongtu@gmail.com>
2023-12-20 13:51:42 +0800

    feat: update port

--- main.go
+++ main.go
@@ -11,13 +11,13 @@ import (
 
 func main() {
 	// Define a flag for the port
-	port := flag.Int("port", 8080, "Port to listen on")
+	port := flag.Int("port", 8880, "Port to listen on")
 	flag.Parse()
 
 	// Create a new Gin router
 	router := gin.Default()
 	api.Register(router)
 
-	// Run the server on port 8080
+	// Run the server on port 8880
 	router.Run(fmt.Sprintf(":%d", *port))
 }
